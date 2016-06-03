2e4ab7e9fdfb545a040a4b39f4484d716bd2dda6
glegoux <gilles.legoux@gmail.com>
2016-06-03 09:18:39 +0200

    Add coding style

--- java-mongo/src/test/java/org/mongo/AbstractTest.java
+++ java-mongo/src/test/java/org/mongo/AbstractTest.java
@@ -10,63 +10,63 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
 public abstract class AbstractTest {
-	
-	public static final String TEST_DB_NAME = "java_mongo_test";
-    private final MongoClient mongoClient;
-    private final Morphia morphia = new Morphia();
-    private DB db;
-    private Datastore ds;
-	
-    protected AbstractTest() {
-        try {
-            mongoClient = new MongoClient(new MongoClientURI(System.getProperty("MONGO_URI", "mongodb://127.0.0.1:27017")));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+
+  public static final String TEST_DB_NAME = "java_mongo_test";
+  private final MongoClient mongoClient;
+  private final Morphia morphia = new Morphia();
+  private DB db;
+  private Datastore ds;
+
+  protected AbstractTest() {
+    try {
+      mongoClient = new MongoClient(new MongoClientURI(System.getProperty("MONGO_URI", "mongodb://127.0.0.1:27017")));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
-    
-    @Before
-    public void setUp() {
-        setDb(getMongoClient().getDB(TEST_DB_NAME));
-        setDs(getMorphia().createDatastore(getMongoClient(), getDb().getName()));
-        cleanup();
+  }
+
+  @Before
+  public void setUp() {
+    setDb(getMongoClient().getDB(TEST_DB_NAME));
+    setDs(getMorphia().createDatastore(getMongoClient(), getDb().getName()));
+    cleanup();
+  }
+
+  @After
+  public void tearDown() {
+    cleanup();
+    getMongoClient().close();
+  }
+
+  protected void cleanup() {
+    DB db = getDb();
+    if (db != null) {
+      db.dropDatabase();
     }
+  }
 
-    @After
-    public void tearDown() {
-        cleanup();
-        getMongoClient().close();
-    }
-    
-    protected void cleanup() {
-        DB db = getDb();
-        if (db != null) {
-            db.dropDatabase();
-        }
-    }
+  public DB getDb() {
+    return db;
+  }
 
-	public DB getDb() {
-		return db;
-	}
+  public void setDb(DB db) {
+    this.db = db;
+  }
 
-	public void setDb(DB db) {
-		this.db = db;
-	}
-	
-    public Morphia getMorphia() {
-        return morphia;
-    }
+  public Morphia getMorphia() {
+    return morphia;
+  }
 
-	public Datastore getDs() {
-		return ds;
-	}
+  public Datastore getDs() {
+    return ds;
+  }
 
-	public void setDs(Datastore ds) {
-		this.ds = ds;
-	}
+  public void setDs(Datastore ds) {
+    this.ds = ds;
+  }
 
-	public MongoClient getMongoClient() {
-		return mongoClient;
-	}
+  public MongoClient getMongoClient() {
+    return mongoClient;
+  }
 
 }
--- java-mongo/src/test/java/org/mongo/AppTest.java
+++ java-mongo/src/test/java/org/mongo/AppTest.java
@@ -8,30 +8,30 @@ import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 
 public class AppTest extends AbstractTest {
-	
-	@Test
-	public void testInsert() {
-		getDs().save(new User("gilles"));
-	}
-	
-	@Entity("users")
-    private static final class User {
-		
-        @Id
-        private ObjectId id;
-        private String name;
 
-        private User() {}
+  @Test
+  public void testInsert() {
+    getDs().save(new User("gilles"));
+  }
 
-        private User(final String name) {
-            this.name = name;
-        }
+  @Entity("users")
+  private static final class User {
 
-        @Override
-        public String toString() {
-            return format("User{name='%s'}", name);
-        }
-        
+    @Id
+    private ObjectId id;
+    private String name;
+
+    private User() {}
+
+    private User(final String name) {
+      this.name = name;
     }
-	
+
+    @Override
+    public String toString() {
+      return format("User{name='%s'}", name);
+    }
+
+  }
+
 }
